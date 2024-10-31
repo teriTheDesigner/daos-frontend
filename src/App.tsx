72fc565e10c681f7a739eea94a7d9c607b0d23c4
teriTheDesigner <tere1109@stud.kea.dk>
2024-10-31 19:53:41 +0100

    review section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import IndexHeader from "./components/IndexHeader/IndexHeader";
 import Nav from "./components/Nav/Nav";
+import UserReviews from "./components/UserReviews/UserReviews";
 
 function App() {
   return (
     <>
       <Nav />
       <IndexHeader />
+      <UserReviews />
     </>
   );
 }
